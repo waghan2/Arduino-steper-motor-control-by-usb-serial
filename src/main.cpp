eb302c1bd6ded18ca226eb7d797df92f6542dfc8
waghan2 <wagnerufpr@gmail.com>
2022-02-02 10:50:07 -0300

    Atualização

--- src/main.cpp
+++ src/main.cpp
@@ -19,15 +19,78 @@ int stpm1 = 500;
 int stpm2 = 500;
 int acm1 = 5000;
 int acm2 = 1000;
-String movimento = "vm1=1800;vm2=800;stpm1=500;stpm2=500;acm1=5000;acm2=1000;";
+String mov1, mov2, mov3, move4, move5;
+String allmoves[] = {"mov1{vm1=1800;vm2=800;stpm1=500;stpm2=500;acm1=5000;acm2=1000;}", 
+"M2", 
+"M3", 
+"M4",
+"M5"};
 AccelStepper stepper1(1, MOTOR_A_STEP_PIN, MOTOR_A_DIR_PIN);
 AccelStepper stepper2(1, MOTOR_B_STEP_PIN, MOTOR_B_DIR_PIN);
+
+enum moves
+{
+  mov1,
+  mov2,
+  mov3,
+  mov4,
+  mov5,
+  mov6,
+  mov7,
+  mov8,
+  mov9,
+  mov10
+};
+
+void SetMov(String move)
+{
+  if (move.startsWith("mov1"))
+  {
+    move.replace("mov1", "");
+    move.replace("{", "");
+    move.replace("}", "");
+    movimento1 = move;
+    Serial.println(movimento1);
+  }
+  if (move.startsWith("mov2"))
+  {
+    move.replace("mov2", "");
+    move.replace("{", "");
+    move.replace("}", "");
+    movimento2 = move;
+    Serial.println(movimento2);
+  }
+  if (move.startsWith("mov3"))
+  {
+    move.replace("mov3", "");
+    move.replace("{", "");
+    move.replace("}", "");
+    movimento3 = move;
+    Serial.println(movimento3);
+  }
+  if (move.startsWith("mov4"))
+  {
+    move.replace("mov4", "");
+    move.replace("{", "");
+    move.replace("}", "");
+    movimento4 = move;
+    Serial.println(movimento4);
+  }
+  if (move.startsWith("mov5"))
+  {
+    move.replace("mov5", "");
+    move.replace("{", "");
+    move.replace("}", "");
+    movimento5 = move;
+    Serial.println(movimento5);
+  }
+}
 
 void setup()
 {
   Serial.begin(115200);
   stepper1.setMaxSpeed(5000.0);    // em passos por segundo
-  stepper1.setMinPulseWidth(20);  // em microsegundos
+  stepper1.setMinPulseWidth(20);   // em microsegundos
   stepper1.setAcceleration(300.0); // em passos por segundo²
   stepper1.moveTo(1000);           // em passos
 
@@ -41,7 +104,7 @@ void voltar()
   stepper1.moveTo(0);
   stepper2.moveTo(0);
 }
-void lerinstru(String movimento)
+void SetVarMoves(String movimento)
 {
   set = true;
 
@@ -82,52 +145,18 @@ void lerinstru(String movimento)
       Serial.println("acm2=" + String(acm2));
     }
   }
+}
 
-  // {int pos = movimento.indexOf(";");
-  // String aux = movimento.substring(0, pos);
-  // movimento = movimento.substring(pos + 1);
-  // pos = aux.indexOf("=");
-  // String var = aux.substring(0, pos);
-  // aux = aux.substring(pos + 1);
-  // int val = aux.toInt();
-  // if (var == "vm1")
-  // {
-  //   vm1 = val;
-  //   Serial.println("vm1=" + String(vm1));
-  // }
-  // if (var == "vm2")
-  // {
-  //   vm2 = val;
-  //   Serial.println("vm2=" + String(vm2));
-  // }
-  // if (var == "stpm1")
-  // {
-  //   stpm1 = val;
-  //   Serial.println("stpm1=" + String(stpm1));
-  // }
-  // if (var == "stpm2")
-  // {
-  //   stpm2 = val;
-  //   Serial.println("stpm2=" + String(stpm2));
-  // }
-  // if (var == "acm1")
-  // {
-  //   acm1 = val;
-  //   Serial.println("acm1=" + String(acm1));
-  // }
-  // if (var == "acm2")
-  // {
-  //   acm2 = val;
-  //   Serial.println("acm2=" + String(acm2));
-  // }}
-
-  Serial.println(movimento);
-  if (movimento.length() > 0)
-  { // se ainda houver instruções
-    // set = true;
-    // lerinstru(movimento);
-    // voltar();
-  }
+void executeMove()
+{
+  stepper1.setSpeed(vm1);
+  stepper1.setAcceleration(acm1);
+  stepper1.moveTo(stpm1);
+  stepper2.setSpeed(vm2);
+  stepper2.setAcceleration(acm2);
+  stepper2.moveTo(stpm2);
+  stepper1.run();
+  stepper2.run();
 }
 
 void loop()
@@ -137,7 +166,13 @@ void loop()
   {
     movimento = Serial.readString();
     Serial.println(movimento);
-    lerinstru(movimento);
+    SetMov(movimento);
+    SetVarMoves(movimento);
+  }
+
+  if (movimento1.length() > 0)
+  {
+    movimento = movimento1;
   }
 
   if (set == true)
@@ -150,7 +185,6 @@ void loop()
     stepper2.setSpeed(acm2);
     movimento = "";
     set = false;
-
   }
 
   if (stepper1.distanceToGo() == 0)
